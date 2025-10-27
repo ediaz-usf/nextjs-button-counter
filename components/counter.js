5745348d640473c2eb70550bd848b30a6643d3e8
Ernesto Diaz <diaz37@usf.edu>
2025-10-27 00:53:45 -0400

    added comments

--- components/counter.js
+++ components/counter.js
@@ -16,17 +16,19 @@ export default function Counter({ initialCount = 0, initialStep = 1, showStepInp
     return (
         <>
             <div className='counter'>
-
+                {/* Display current count */}
                 <div className='count-display' aria-live='polite'>
                     Count: {count}
                 </div>
 
+                {/* Buttons to increment, decrement, and reset the count */}
                 <div id='buttons'>
                     <button onClick={increment}>+{step}</button>
                     <button onClick={decrement} disabled={count - step < 0}>-{step}</button>
                     <button onClick={reset}>Reset</button>
                 </div>
 
+                {/* Optional input to set the step value */}
                 {showStepInput && (
                 <div>
                 <label htmlFor="step">Step: </label>
